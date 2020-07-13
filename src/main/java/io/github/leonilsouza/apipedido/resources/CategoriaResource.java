9f424692cb4a498a24710641d4e2650113611d51
Leonil Souza <leonilsouza@gmail.com>
2020-07-12 21:14:20 -0400

    Inclusão H2

--- src/main/java/io/github/leonilsouza/apipedido/resources/CategoriaResource.java
+++ src/main/java/io/github/leonilsouza/apipedido/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package io.github.leonilsouza.apipedido.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.leonilsouza.apipedido.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escitorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
