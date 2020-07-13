b97016b0babc4d1465377e2cec4957ecbca61867
Leonil Souza <leonilsouza@gmail.com>
2020-07-13 15:21:19 -0400

    Associação Muitos para Muitos de Produtos e Categoria

--- src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
+++ src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
@@ -1,13 +1,48 @@
 package io.github.leonilsouza.apipedido;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiPedidoApplication {
+import io.github.leonilsouza.apipedido.domain.Categoria;
+import io.github.leonilsouza.apipedido.domain.Produto;
+import io.github.leonilsouza.apipedido.repositories.CategoriaRepository;
+import io.github.leonilsouza.apipedido.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class ApiPedidoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
