6aa24dee889636878d37dfd0038cc3fa40f6b8e8
Leonil Souza <leonilsouza@gmail.com>
2020-07-13 18:25:01 -0400

    Ajuste no endpoint /Categoria/{id}

--- src/main/java/io/github/leonilsouza/apipedido/domain/Produto.java
+++ src/main/java/io/github/leonilsouza/apipedido/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
+	@JsonBackReference // Omite a Lista de Categoria p/ cada Produto e evita o cíclico
 	@ManyToMany
 	@JoinTable(name = "produto_categoria", 
 		joinColumns = @JoinColumn(name = "produto_id"),
