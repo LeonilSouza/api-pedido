f28fccdde7ea52288d8a207feae928a2fb2ef135
Leonil Souza <leonilsouza@gmail.com>
2020-07-13 21:28:42 -0400

    Estado e Cidade

--- src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
+++ src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.leonilsouza.apipedido.domain.Categoria;
+import io.github.leonilsouza.apipedido.domain.Cidade;
+import io.github.leonilsouza.apipedido.domain.Estado;
 import io.github.leonilsouza.apipedido.domain.Produto;
 import io.github.leonilsouza.apipedido.repositories.CategoriaRepository;
+import io.github.leonilsouza.apipedido.repositories.CidadeRepository;
+import io.github.leonilsouza.apipedido.repositories.EstadoRepository;
 import io.github.leonilsouza.apipedido.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ApiPedidoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPedidoApplication.class, args);
@@ -40,9 +50,22 @@ public class ApiPedidoApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlãndia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo ", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c1, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/io/github/leonilsouza/apipedido/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package io.github.leonilsouza.apipedido.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.leonilsouza.apipedido.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
