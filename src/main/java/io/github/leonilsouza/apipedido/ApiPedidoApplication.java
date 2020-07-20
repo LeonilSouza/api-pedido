7782d03e243ae4b5d12f8bbec544f9bcd01eaebc
Leonil Souza <leonilsouza@gmail.com>
2020-07-19 22:30:07 -0400

    Pedido, Pagamento e EstadoPgamento

--- src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
+++ src/main/java/io/github/leonilsouza/apipedido/ApiPedidoApplication.java
@@ -1,5 +1,6 @@
 package io.github.leonilsouza.apipedido;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import io.github.leonilsouza.apipedido.domain.Cidade;
 import io.github.leonilsouza.apipedido.domain.Cliente;
 import io.github.leonilsouza.apipedido.domain.Endereco;
 import io.github.leonilsouza.apipedido.domain.Estado;
+import io.github.leonilsouza.apipedido.domain.Pagamento;
+import io.github.leonilsouza.apipedido.domain.PagamentoComBoleto;
+import io.github.leonilsouza.apipedido.domain.PagamentoComCartao;
+import io.github.leonilsouza.apipedido.domain.Pedido;
 import io.github.leonilsouza.apipedido.domain.Produto;
+import io.github.leonilsouza.apipedido.domain.enums.EstadoPagamento;
 import io.github.leonilsouza.apipedido.domain.enums.TipoCliente;
 import io.github.leonilsouza.apipedido.repositories.CategoriaRepository;
 import io.github.leonilsouza.apipedido.repositories.CidadeRepository;
 import io.github.leonilsouza.apipedido.repositories.ClienteRepository;
 import io.github.leonilsouza.apipedido.repositories.EnderecoRepository;
 import io.github.leonilsouza.apipedido.repositories.EstadoRepository;
+import io.github.leonilsouza.apipedido.repositories.PagamentoRepository;
+import io.github.leonilsouza.apipedido.repositories.PedidoRepository;
 import io.github.leonilsouza.apipedido.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class ApiPedidoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPedidoApplication.class, args);
@@ -89,6 +103,22 @@ public class ApiPedidoApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("30/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("10/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
