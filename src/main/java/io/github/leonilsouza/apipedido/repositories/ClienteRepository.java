a7006c57169d93fb5fb61c97788683ee8d135b5f
Leonil Souza <leonilsouza@gmail.com>
2020-10-09 21:15:39 -0400

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/io/github/leonilsouza/apipedido/repositories/ClienteRepository.java
+++ src/main/java/io/github/leonilsouza/apipedido/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package io.github.leonilsouza.apipedido.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.leonilsouza.apipedido.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)// Melhora a performance de pesquisa no banco
+	Cliente findByEmail(String email);
 
 }
